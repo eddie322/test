40593eebec5f38e5925856a2bfb657dd75c0ec0d
Eddie322 <edgargasanov@mail.ru>
2023-06-16 17:29:30 +0400

    test

---
+++ blog/api/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Post
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    posts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    
+    class Meta:
+        model = User
+        fields = ['id',  'username', 'first_name', 'last_name', 'posts']
+
+
+
+class PostSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'body', 'owner']
